b130eb049a0a770556d632b2159f4cd716379a71
Thedas Sri Harisha <thedassriharisha@gmail.com>
2025-08-04 22:30:12 +0530

    update

--- frontend/src/utils/axiosConfig.ts
+++ frontend/src/utils/axiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://alwon.onrender.com',
 });
 
 instance.interceptors.request.use((config) => {
